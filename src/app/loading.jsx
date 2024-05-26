52dd01d7d97373dc8cb6589ae180b2091fb6fc6d
hasan814 <h.mousavi910@gmail.com>
2024-05-26 23:02:55 +0330

    Added Loading Effect

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,17 +4,19 @@ import { RotatingLines } from "react-loader-spinner";
 
 const Loading = () => {
   return (
-    <RotatingLines
-      visible={true}
-      height="96"
-      width="96"
-      color="grey"
-      strokeWidth="5"
-      animationDuration="0.75"
-      ariaLabel="rotating-lines-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-    />
+    <div className="flex justify-center">
+      <RotatingLines
+        visible={true}
+        height="96"
+        width="96"
+        color="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        ariaLabel="rotating-lines-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </div>
   );
 };
 
--- src/utils/FetchData.js
+++ src/utils/FetchData.js
@@ -4,7 +4,8 @@ export const fetchData = async (genre) => {
   const response = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US`
+    }?api_key=${API_KEY}&language=en-US`,
+    { next: { revalidate: 10 } }
   );
   const data = await response.json();
   if (!response.ok) throw new Error("Failed to fetch Data");
