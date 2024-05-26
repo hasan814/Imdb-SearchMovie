98f07c5118e008ed8ddc5b78c8428d7039e4733d
hasan814 <h.mousavi910@gmail.com>
2024-05-26 20:59:12 +0330

    Added Aboutpage Section

---
+++ src/app/about/page.jsx
@@ -0,0 +1,7 @@
+import AboutPage from "@/components/templates/AboutPage";
+
+const About = () => {
+  return <AboutPage />;
+};
+
+export default About;
---
+++ src/components/templates/AboutPage.jsx
@@ -0,0 +1,36 @@
+const AboutPage = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p className="text-justify">
+        Welcome to Our movie database website! We are a team of passionate movie
+        enthusiastis who have come together to create a one-stop destination for
+        all your movie-related needs.
+      </p>
+      <p className="text-justify">
+        Our Website is designed to provide you with a comprehensive database of
+        movies from all around the world, along with the latest news,
+        reviews,and trailers.Our movie database is constantly updated with new
+        releases, ensuring that you have access to the latest and greatest in
+        the world of cinema. You can search for movies by title, director,
+        actor, genre, or realse date, making it easy to find the prefect movie
+        for any occasion
+      </p>
+      <p className="text-justify">
+        In addition to our extensive movie database, we also offer a platform
+        for movie lovers to connect and share thier thoughts on the latest
+        releases. Our community section includes a forum where you can discuss
+        your favorite films with like-minded individuals and read reviews and
+        rating from other users. We also have a section dedicated to movie news
+        and trailers, keeping you updated with the latest happenings in the
+        world of cinema.Thank you for visiting our website and we hope you enjoy
+        time bowsing though our movie database.If you have any feedback or
+        suggestions, please feel free to contact us.We are always looking for
+        ways to improve and enhance the user experience on our website.Happy
+        Browsing
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
